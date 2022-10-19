e33b32b8a168bb3bc71984fda6793b7ad198ab27
Nicolae Novac <flaviusnicolaen@gmail.com>
2022-10-20 00:21:57 +0300

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,87 +1,83 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
 (require 'package)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/contrib"))
+
 (require 'org-checklist)
+(require 'dired-x)
 
 (setq org-directory
-      (cond ((string= (system-name) "PE426MF") "C:/Users/flavi/Dropbox/GTD")
-	    ; maybe Fresenius OneDrive is better?
-	    ((string= (system-name) "7BKQKQ2") "C:/Users/nicolae.novac/Dropbox/GTD Work")
-	    ((string= (system-name) "HP861G2") "C:/Users/nicolae.novac/Dropbox/GTD Work")
+      (cond ((string= (system-name) "PE426MF") "D:/MEGA/doc")
 	    (t "C:/")))
-(setq org-agenda-files
-      (concat org-directory "/agenda-files"))
-
-(require 'dired-x)
+(setq org-agenda-files "~/.emacs.d/agenda-files")
 (setq-default dired-omit-files-p t) ; Buffer-local variable
+(setq ps-printer-name t)
+(setq ps-lpr-command "C:/Program Files/gs/gs9.53.3/bin/gswin64c.exe")
+(setq ps-lpr-switches '("-q" "-dNOPAUSE" "-dBATCH"
+                        "-sDEVICE=mswinpr2"
+                        "-sPAPERSIZE=a4"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(calendar-week-start-day 1)
  '(fill-column 80)
  '(initial-buffer-choice org-directory)
- '(initial-frame-alist (quote ((height . 70) (width . 200) (top . 50) (left . 1))))
+ '(initial-frame-alist '((height . 70) (width . 200) (top . 50) (left . 1)))
  '(org-agenda-custom-commands
-   (quote
-    ((" " "GTD"
+   '(("g" "GTD"
       ((agenda ""
-	       ((org-agenda-span
-		 (quote day))
+	       ((org-agenda-span 'week)
 		(org-agenda-overriding-header "Scheduled")))
        (stuck ""
 	      ((org-agenda-overriding-header "Stuck projects")))
        (todo "NEXT"
 	     ((org-agenda-overriding-header "Not scheduled")
 	      (org-agenda-skip-function
-	       (quote
-		(org-agenda-skip-entry-if
-		 (quote scheduled)
-		 (quote deadline))))))
-       (todo "HOLD|WAITING"
-	     ((org-agenda-overriding-header "Suspended"))))
-      nil))))
- '(org-attach-store-link-p (quote attached))
+	       '(org-agenda-skip-entry-if 'scheduled 'deadline))))
+       (todo "HOLD|WAIT"
+	     ((org-agenda-overriding-header "Suspended")))
+       (todo "PROJ"
+	     ((org-agenda-overriding-header "Active projects"))))
+      nil)))
+ '(org-attach-store-link-p 'attached)
  '(org-capture-templates
-   (quote
-    (("r" "Reference" entry
-      (file+headline "../org/ref.org" "Captured")
-      "* TODO REFILE: %?
-%U")
+   '(("e" "Event" entry
+      (file+olp+datetree "events.org")
+      "* %?
+%T" :time-prompt t)
      ("t" "Task" entry
       (file+headline "gtd.org" "Captured")
-      "* TODO REFILE: %?
-%U" :empty-lines 1)
-     ("j" "Journal" entry
-      (file+olp+datetree "journal.org")
-      "** %<%H:%M> %?
-"))))
+      "* TODO REFILE TASK: %?
+%U" :empty-lines 1)))
+ '(org-capture-use-agenda-date t)
  '(org-clock-idle-time 60)
  '(org-clock-in-resume t)
  '(org-clock-out-remove-zero-time-clocks t)
- '(org-clock-persist (quote clock))
+ '(org-clock-persist 'clock)
  '(org-clock-report-include-clocking-task t)
  '(org-columns-default-format
    "%7TODO(State) %ITEM(Task) %2PRIORITY(P) %5Effort(Est.){:} %5CLOCKSUM(Time){:}")
  '(org-enforce-todo-dependencies t)
  '(org-habit-graph-column 60)
- '(org-log-done (quote time))
+ '(org-hide-leading-stars t)
+ '(org-log-done 'time)
  '(org-log-into-drawer t)
- '(org-log-reschedule (quote note))
+ '(org-log-reschedule 'note)
  '(org-modules
-   (quote
-    (org-bbdb org-bibtex org-docview org-habit org-info org-irc org-mhe org-rmail org-w3m org-learn org-toc)))
- '(org-refile-allow-creating-parent-nodes (quote confirm))
- '(org-refile-use-outline-path (quote file))
- '(org-stuck-projects (quote ("/TODO|PROJ" ("NEXT") nil "")))
+   '(org-bbdb org-bibtex org-docview org-habit org-info org-irc org-mhe org-rmail org-w3m org-learn org-toc))
+ '(org-refile-allow-creating-parent-nodes 'confirm)
+ '(org-refile-use-outline-path 'file)
+ '(org-startup-folded t)
+ '(org-stuck-projects '("/TODO|PROJ" ("NEXT" "WAIT") nil ""))
+ '(org-todo-keywords
+   '((sequence "PROJ(p)" "TODO(t)" "NEXT(n)" "HOLD(h@/!)" "WAIT(w@/!)" "|" "DONE(d)" "CANCELLED(c@/!)")))
  '(org-todo-repeat-to-state "NEXT")
- '(package-selected-packages (quote (yasnippet))))
+ '(package-selected-packages '(magit yasnippet org helm f emacsql-sqlite3)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -89,7 +85,6 @@
  ;; If there is more than one, they won't work right.
  )
  
-
 (global-set-key (kbd "<f11>") 'org-clock-goto)
 (global-set-key (kbd "<f12>") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
@@ -103,19 +98,16 @@
 ;; Resume clocking task when emacs is restarted
 (org-clock-persistence-insinuate)
 
-(setq org-columns-default-format "%50ITEM(Task) %2PRIORITY %10Effort(Effort){:} %10CLOCKSUM")
-
 (setq org-global-properties
-      '(("Effort_ALL" .
-         "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")))
+      '(("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")))
 
 (setq org-todo-keywords
-      (quote ((sequence "PROJ(p)" "TODO(t)" "NEXT(n)" "HOLD(h@/!)" "WAITING(w@/!)" "|" "DONE(d@/!)" "CANCELLED(c@/!)" "APP(a)"))))
+      (quote ((sequence "PROJ(p)" "TODO(t)" "NEXT(n)" "HOLD(h@/!)" "WAIT(w@/!)" "|" "DONE(d/!)" "CANCELLED(c@/!)" "APP(a)"))))
 
 (setq org-todo-keyword-faces
       (quote (("TODO" :foreground "red" :weight bold)
               ("NEXT" :foreground "blue" :weight bold)
               ("DONE" :foreground "forest green" :weight bold)
               ("HOLD" :foreground "orange" :weight bold)
-              ("WAITING" :foreground "orange" :weight bold)
+              ("WAIT" :foreground "orange" :weight bold)
               ("CANCELLED" :foreground "forest green" :weight bold))))
